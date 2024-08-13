52826656c6973d4e64ff2cce69d5946c41517b19
owner <https://github.com/Animemanji/ads_js>
2024-08-14 02:01:58 +0530

    update

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+// pages/ad.js
+import { useEffect, useState } from 'react';
+
+export default function Ad() {
+  const [imageSrc, setImageSrc] = useState('');
+
+  useEffect(() => {
+    setImageSrc(`https://picsum.photos/345/200?random=${new Date().getTime()}`);
+  }, []);
+
+  return (
+    <div>
+      <h1>Your Ad</h1>
+      <img src={imageSrc} alt="Random Image" />
+    </div>
+  );
+}
